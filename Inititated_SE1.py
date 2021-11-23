06982f51ca69c05bcae81572ce2bf640e047f56c
Sank <55155638+kattelsATWIT@users.noreply.github.com>
2021-11-22 19:37:07 -0500

    1st_python_program_to_git

---
+++ Inititated_SE1.py
@@ -0,0 +1,9 @@
+# By Sank
+import math 
+#importing Math module to use pi in this specific program. 
+r = float(input("Enter R"))
+# input here is used to take input from the user and float converts the so string 'r' to floating value
+Area = math.pi*r*r
+# Area is used to compute the area of a circle
+print(str(Area))
+#finally the use of 'str' here is used to print the computed area as string.
